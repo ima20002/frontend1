917ebb39929052936c4f6926ddd7a8335958c57b
ima20002 <ima20002@byui.edu>
2021-03-14 14:59:01 +0900

    commit

--- lesson9/js/windchill.js
+++ lesson9/js/windchill.js
@@ -1,7 +1,7 @@
-const tempNumber = parseFloat(document.getElementById("temp").textContent);
+const tempNumber = parseFloat(document.getElementById("current_temp").textContent);
 console.log(tempNumber);
 
-const speedNumber = parseFloat(document.getElementById("speed").textContent);
+const speedNumber = parseFloat(document.getElementById("wind_speed").textContent);
 console.log(speedNumber);
 
 let windchill = 35.74 + (0.6215 * tempNumber) - (35.75 * Math.pow(speedNumber, 0.16)) + (0.4275 * tempNumber * Math.pow(speedNumber, 0.16));
@@ -10,8 +10,8 @@ windchill = Math.round(windchill);
 console.log(windchill);
 
 if(tempNumber <= 50 && speedNumber > 3){
-    document.getElementById("chill").textContent = "Windchill:  " + windchill + "\xB0F";ß
+    document.getElementById("chill").textContent = windchill;
 }
 else {
-    document.getElementById("chill").textContent = "Windchill: N/A";
+    document.getElementById("chill").textContent = "N/A";
 }
