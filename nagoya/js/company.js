d7a3eb4ab76b0db15459cabf73db039ad3d7e3a2
ima20002 <ima20002@byui.edu>
2021-04-08 07:19:28 +0900

    commit

--- nagoya/js/company.js
+++ nagoya/js/company.js
@@ -1,4 +1,4 @@
-const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+const requestURL = 'https://raw.githubusercontent.com/ima20002/ima20002.github.io/master/nagoya/business.json';
 
 fetch(requestURL)
   .then(function (response) {
@@ -8,7 +8,9 @@ fetch(requestURL)
 
     const business = jsonObject['business'];
     
-      let card = document.createElement('container');
+    for (let i = 0; i < business.length; i++ ) {
+
+      let card = document.createElement('section');
 
       let name = document.createElement('h3');
       let contact = document.createElement('p');
@@ -27,5 +29,6 @@ fetch(requestURL)
       card.appendChild(image);
 
       document.querySelector('div.cards').appendChild(card); 
+    }
   });
 
